6ef71ed600fb6d7faaad749d07c4a1e9fab7238f
Leonardo Lima <leo2342005@gmail.com>
2025-08-17 21:45:26 -0300

    refactor: adding a message to certification's not blank validation.

--- src/main/java/com/vouchers/dto/certification/CreateCertificationDTO.java
+++ src/main/java/com/vouchers/dto/certification/CreateCertificationDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCertificationDTO(
-        @NotBlank
+        @NotBlank(message = "Code is required")
         @Size(min = 6, max = 6, message = "Code must be 6 characters")
         String code
 ) {
