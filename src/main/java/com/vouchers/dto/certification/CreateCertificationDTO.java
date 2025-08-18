53458a25be794ad828622f5431996d4bb43d20cf
Leonardo Lima <leo2342005@gmail.com>
2025-08-17 21:44:42 -0300

    refactor: adding a not blank validation to certification's name.

--- src/main/java/com/vouchers/dto/certification/CreateCertificationDTO.java
+++ src/main/java/com/vouchers/dto/certification/CreateCertificationDTO.java
@@ -1,8 +1,10 @@
 package com.vouchers.dto.certification;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCertificationDTO(
+        @NotBlank
         @Size(min = 6, max = 6, message = "Code must be 6 characters")
         String code
 ) {
